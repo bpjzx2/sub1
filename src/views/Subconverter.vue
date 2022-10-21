5d669e46fde62322bda60926103af6de0f82873a
bpjzx2 <102860208+bpjzx2@users.noreply.github.com>
2022-10-21 21:44:34 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -31,19 +31,16 @@
               </el-form-item>
 	
 	<el-form-item label="后端地址:">
-                  
-	
-	 <el-autocomplete
-                   
-<select style="width: 100%">
-  		<option value="https://sub.bpjzx2.workers.dev/sub?" >https://sub.bpjzx2.workers.dev(需要代理，不推荐)</option>
-  		<option value="https://sub.xeton.dev/sub?">https://sub.xeton.dev(推荐)</option>
-  		<option value="https://api.dler.io/sub?">https://api.dler.io(推荐)</option>
-  		<option value="https://sub.789.st/sub?">https://sub.789.st(推荐)</option>
+                  <el-autocomplete
+                    style="width: 100%"
+                    v-model="form.customBackend"
+                    :fetch-suggestions="backendSearch"
+                    placeholder="动动小手，（建议）自行搭建后端服务。例：http://127.0.0.1:25500/sub?"
+                  >
+                    <el-button slot="append" @click="gotoGayhub" icon="el-icon-link">前往项目仓库</el-button>
                   </el-autocomplete>
-
-	</select>
                 </el-form-item>
+ 
               <div v-if="advanced === '2'">
                 
                 <el-form-item label="远程配置:">
@@ -342,7 +339,15 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: "",
+        customBackend: 
+<select style="width: 100%">
+  		<option value="https://sub.bpjzx2.workers.dev/sub?" >https://sub.bpjzx2.workers.dev(需要代理，不推荐)</option>
+  		<option value="https://sub.xeton.dev/sub?">https://sub.xeton.dev(推荐)</option>
+  		<option value="https://api.dler.io/sub?">https://api.dler.io(推荐)</option>
+  		<option value="https://sub.789.st/sub?">https://sub.789.st(推荐)</option>
+                  </el-autocomplete>
+
+	</select>,
         remoteConfig: "",
         excludeRemarks: "",
         includeRemarks: "",
