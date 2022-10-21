28226eff002d08fbb21aab36c78a7c555577f484
bpjzx2 <102860208+bpjzx2@users.noreply.github.com>
2022-10-21 22:45:44 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -35,7 +35,8 @@
 		  placeholder="默认后端地址对需要代理才可访问的网站很不友好，例如GitHub反代"
                     style="width: 100%"
                     v-model="form.customBackend"
-                    :fetch-suggestions="backendSearch" @select="handleSelect">
+                    :fetch-suggestions="backendSearch"
+		    @select="handleSelect">
 	
                     <el-button slot="append" @click="gotoGayhub" icon="el-icon-link">前往项目仓库</el-button>
                   </el-autocomplete>
