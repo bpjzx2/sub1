e604bc8fdcd475314dbacaddcce0f3f6f5257c53
bpjzx2 <102860208+bpjzx2@users.noreply.github.com>
2022-10-21 21:22:44 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -37,6 +37,12 @@
                     :fetch-suggestions="backendSearch"
                     placeholder="动动小手，（建议）自行搭建后端服务。例：http://127.0.0.1:25500/sub?"
                   >
+	<select style="width: 100%">
+  		<option value="https://sub.bpjzx2.workers.dev/sub?" >https://sub.bpjzx2.workers.dev(需要代理，不推荐)</option>
+  		<option value="https://sub.xeton.dev/sub?">https://sub.xeton.dev(推荐)</option>
+  		<option value="https://api.dler.io/sub?">https://api.dler.io(推荐)</option>
+  		<option value="https://sub.789.st/sub?">https://sub.789.st(推荐)</option>
+	</select>
                     <el-button slot="append" @click="gotoGayhub" icon="el-icon-link">前往项目仓库</el-button>
                   </el-autocomplete>
                 </el-form-item>
